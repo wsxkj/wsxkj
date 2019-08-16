b30ee704a800dbe8077da7daddbc6f258fbfdf10
zpj <330273931@qq.com>
2019-08-16 08:51:50 +0800

    Update StoreService.java

--- wsxkj/src/main/java/com/zpj/materials/service/StoreService.java
+++ wsxkj/src/main/java/com/zpj/materials/service/StoreService.java
@@ -1,12 +1,24 @@
 package com.zpj.materials.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.zpj.common.MyPage;
 import com.zpj.materials.entity.Store;
 
 public interface StoreService {
-	
+
+    /*
+     * @MethodName: findStatisticsData
+     * @Description: TODO(根据月份查询总进货数，总金额)
+     * @params [canshu]
+     * @return java.util.Map
+     * @author zpj
+     * @date 2019/8/6 10:53
+    */
+    public Map findStatisticsData(Map canshu);
+    List findMultiData(Map param, Integer page, Integer limit);
+
 	MyPage findPageData(Map param, Integer page, Integer limit);
     /*
      * @MethodName: saveInfo
